db6704319408e8d56ebe713acce85aab7ee7f934
Alwin Esch <alwin.esch@web.de>
2020-01-06 10:36:03 +0100

    rewrite addon source code

--- src/Helpers.h
+++ src/Helpers.h
@@ -28,6 +28,9 @@
 #include <mutex>
 #include <thread>
 
+namespace
+{
+
 inline const char* kodiTranslateLogLevel(const AddonLog logLevel)
 {
   switch (logLevel)
@@ -91,6 +94,62 @@ public:
 private:
   uint64_t m_iTarget;
 };
+
+inline std::string StringFormatV(const char* fmt, va_list args)
+{
+  if (fmt == nullptr)
+    return "";
+
+  int size = 2048;
+  va_list argCopy;
+
+  char *cstr = reinterpret_cast<char*>(malloc(sizeof(char) * size));
+  if (cstr == nullptr)
+    return "";
+
+  while (1)
+  {
+    va_copy(argCopy, args);
+
+    int nActual = vsnprintf(cstr, size, fmt, argCopy);
+    va_end(argCopy);
+
+    if (nActual > -1 && nActual < size) // We got a valid result
+    {
+      std::string str(cstr, nActual);
+      free(cstr);
+      return str;
+    }
+    if (nActual > -1)                   // Exactly what we will need (glibc 2.1)
+      size = nActual + 1;
+    else                                // Let's try to double the size (glibc 2.0)
+      size *= 2;
+
+    char *new_cstr = reinterpret_cast<char*>(realloc(cstr, sizeof(char) * size));
+    if (new_cstr == nullptr)
+    {
+      free(cstr);
+      return "";
+    }
+
+    cstr = new_cstr;
+  }
+
+  free(cstr);
+  return "";
+}
+
+inline std::string StringFormat(const char* fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  std::string str = StringFormatV(fmt, args);
+  va_end(args);
+
+  return str;
+}
+
+} // namespace
 
 namespace ThreadHelpers
 {
